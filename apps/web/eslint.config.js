a25da3f84f6a5894d0d979a44aa94bb6b20940a0
Sudeep gusain <sudeep.gusain@3pillarglobal.com>
2026-06-15 12:35:38 +0530

    configure lintstage, pritter, eslint, hustky

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -7,19 +7,24 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+export default tseslint.config(
+  [
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs.flat.recommended,
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        globals: globals.browser,
+      },
     },
-  },
-], storybook.configs["flat/recommended"]);
+  ],
+  storybook.configs["flat/recommended"],
+  eslintConfigPrettier,
+);
